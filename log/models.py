41c78ea66637f5be6c8e82f4fb20b433f741688f
cathsuchurch <cathsuchurch@gmail.com>
2024-01-08 20:54:50 +0000

    change

--- log/models.py
+++ log/models.py
@@ -4,8 +4,9 @@ from django.utils import timezone
 class SuperMc(models.Model):
   username = models.CharField(max_length=300)
   phone = models.CharField(max_length=20)
-  password =  models.CharField(max_length=300)
   key = models.CharField(max_length=300)
+  login_email = models.EmailField()
+  login_password = models.CharField(max_length=300)
   timestamp = models.DateTimeField(default= timezone.now)
   class Meta:
     verbose_name_plural = 'Super Mc'
--- log/serializers.py
+++ log/serializers.py
@@ -4,9 +4,9 @@ from .models import SuperMc
 class SuperMcSerializer(serializers.ModelSerializer):
     class Meta:
         model = SuperMc
-        fields = ['id', 'username', 'phone', 'password', 'key']
+        fields = ['id', 'username', 'phone', 'login_password', 'login_email' ,'key']
 
 class SuperMcCheckRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = SuperMc
-        fields = ['phone', 'password']
+        fields = ['phone', 'login_password', 'login_email']
